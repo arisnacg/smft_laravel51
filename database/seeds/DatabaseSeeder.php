4ce8f6b5ea089f46b19a2bbe7f3e0d76eef18627
pradiptaagus <igedeaguspradipta@gmail.com>
2018-07-18 15:14:54 +0800

    add migration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,13 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(AdminTableSeeder::class);
+        $this->call(AgamaTableSeeder::class);
+        $this->call(AngkatanTableSeeder::class);
+        $this->call(GolonganDarahTableSeeder::class);
+        $this->call(JenisKelaminTableSeeder::class);
+        $this->call(ProgramStudiTableSeeder::class);
 
         Model::reguard();
     }
